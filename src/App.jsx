5d423101dc845b9cd4a395dd95062c7b59911f1b
surazgaundel <gaundelsuraz99@gmail.com>
2023-03-12 00:07:15 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
   function handleItem(e){
     const task={
       id:item.length===0?1:item[item.length-1].id+1,
-      itemName:listItem
+      itemName:listItem,
+      notCompleted:false
     } 
     setItem([...item,task]);
   }
@@ -21,6 +22,14 @@ function App() {
   function deleteItem(id){
     setItem(item.filter(el=>el.id!==id));
   }
+  
+  function notDone(id){
+    setItem(item.map(el=>{
+      if(el.id===id){
+        return{...el,notCompleted:true};
+      } else { return el;}
+  }))
+}
   return (
     <div className="App"><hr/>
     <h1>NOT TO DO</h1><hr/>
@@ -29,7 +38,7 @@ function App() {
     <div className="itemList">
       {item.length>0?
       item.map(el=>{
-        return(<ul><li onClick={()=>deleteItem(el.id)} style={{cursor:'pointer'}}>{el.itemName}</li></ul>)
+        return(<ul><span><li onClick={()=>deleteItem(el.id)} style={{cursor:'pointer',backgroundColor:el.notCompleted?'green':''}}>{el.itemName}</li><button onClick={()=>notDone(el.id)}>Not Done</button></span></ul>)
       }) :<h3>Clear Your Mind</h3>
   }
     </div>
